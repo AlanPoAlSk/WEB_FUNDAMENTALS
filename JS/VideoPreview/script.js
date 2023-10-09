e4160b95fff3ae2a54cf0422ebd427429c6625c9
AlanPoAlsk <alsk91@gmail.com>
2023-10-09 10:20:36 -0400

    onmouseover-out

---
+++ JS/VideoPreview/script.js
@@ -0,0 +1,12 @@
+console.log("page loaded...");
+
+var x = document.getElementById("myVideo");
+
+function myVideo(element) {
+    element.play();
+}
+
+function stopVideo(element) {
+    element.pause();
+
+}
