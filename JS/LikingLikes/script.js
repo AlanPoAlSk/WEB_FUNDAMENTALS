db60da155d1ae6bf04fbaa83949fd5a443b347c5
AlanPoAlsk <alsk91@gmail.com>
2023-10-10 15:20:03 -0400

    Liking likes exercise

---
+++ JS/LikingLikes/script.js
@@ -0,0 +1,28 @@
+var like = 9 
+var addlikes= document.querySelector(".likes1")
+
+function increase() {
+    like++;
+    addlikes.innerHTML = like + " like(s) ";
+
+}
+
+var likeB = 12 
+var addlikesB= document.querySelector(".likes2")
+
+function increaseB() {
+    likeB++;
+    addlikesB.innerHTML = likeB + " like(s) ";
+
+}
+
+
+var likeC = 9 
+var addlikesC= document.querySelector(".likes3")
+
+function increaseC() {
+    likeC++;
+    addlikesC.innerHTML = likeC + " like(s) ";
+
+}
+
